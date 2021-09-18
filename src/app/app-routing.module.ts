a79bfbb132d9bec6f346f86c6b70b128b1ba448f
atomo <atomotechnology@gmail.com>
2021-09-18 00:19:13 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: `/home`,
+    pathMatch: 'full',
+  },{
+    path: 'home',
+    component: HomeComponent,
+     data: {title: 'home'}
+  }
+  ,{
+    path: 'login',
+    component: AccountComponent,
+     data: {title: 'login'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
+import { BrowserComponent } from './components/browser/browser.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountComponent,
+    BrowserComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
